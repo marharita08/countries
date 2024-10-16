78d33a2a5e0538e14c6a7f5e2d77a03234abc56e
marharita08 <rozgon.margarita08@gmail.com>
2024-10-16 12:25:27 +0300

    set prettier

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,10 @@
-const express = require('express');
+import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
-app.get('/', (req, res) => {
-  res.send('Hello, Express.js!');
+app.get("/", (req, res) => {
+  res.send("Hello, Express.js!");
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
