8b76a6951ce7ab2c3d2f027217133b373d119252
marharita08 <rozgon.margarita08@gmail.com>
2024-12-10 17:30:38 +0200

    add default navigation to countries

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import { Countries } from "./components/countries/countries";
 import { CountryInfo } from "./components/country-info/country-info";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/countries" element={<Countries />} />
         <Route path="/countries/:code" element={<CountryInfo />} />
+        <Route path="*" element={<Navigate to="/countries" />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
