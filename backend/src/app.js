17ab4243f1886e51f504ce1a865ed610999aaf2f
marharita08 <rozgon.margarita08@gmail.com>
2024-12-10 18:26:15 +0200

    add vercel.json

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,14 @@
 import express from "express";
 import cors from "cors";
 
-import { env } from "./configs/envConfigs.js";
 import contriesRoutes from "./routes/countries.routes.js";
 import { errorHandler } from "./middlewares/error-handler.middleware.js";
 
 const app = express();
-const PORT = env.PORT;
 
 app.use(cors());
 
 app.use("/countries", contriesRoutes);
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+export default app;
